26d5024ec75781ce1468d0f5e7d40e62ee08abca
yarr228 <kaa.ka.12@mail.ru>
2024-03-27 16:08:30 +0300

    #1 Начало разработки главной страницы

---
+++ html/main_page.php
@@ -0,0 +1,22 @@
+<html lang="en">
+<head>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300..700&display=swap" rel="stylesheet">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+    <?php 
+        echo file_get_contents("../css/main_page_styles.css");
+    ?>
+    </style>
+    <title>Musicly</title>
+</head>
+<body>
+    <div class="main_page_block">
+        <div class="main_page_title">
+            <h1>Shashki</h1>
+        </div>
+    </div>
+</body>
+</html>
